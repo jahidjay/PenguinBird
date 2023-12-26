e6dce22601d6612ad2a0e52a8bdf7b9fff863f80
Jahid Vinact <jahidyasin2010@gmail.com>
2023-12-26 17:37:01 +0600

    Added

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -1,11 +1,62 @@
-extends Node
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
+class_name BirdCharacter
+@export var gravity = 900.0
+@export var jump_force = -300
+@export var rotation_speed = 2
+
+@onready var animation_player = $AnimationPlayer
+
+var max_speed = 400
+var is_started = false 
+var should_process_input = true
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
 func _ready():
-	pass # Replace with function body.
+	velocity = Vector2.ZERO
+	animation_player.play("Idle")
+	
+	
+func _physics_process(delta):
+	if Input.is_action_just_pressed("jump"):
+		if !is_started:
+			animation_player.play("flap_wings")
+			is_started = true 
+		jump()
+		
+	if !is_started:
+		return
+		velocity.y += gravity * delta
+		velocity.y = min(velocity.y, max_speed)
+		
+		move_and_collide(velocity * delta)
+		
+		rotate_Bird()
+		
+
+		
+func jump():
+	velocity.y = jump_force 
+	rotation = deg_to_rad(-30)
+	
+	
+func rotate_Bird():
+	if velocity.y > 0 && rad_to_deg(rotation) < 90:
+		rotation += rotation_speed * deg_to_rad(1)
+		
+	elif velocity.y < 0 && rad_to_deg(rotation) > -30:
+		rotation -= rotation_speed * deg_to_rad(1)
+		
+func stop():
+	animation_player.stop()
+	gravity = 0
+	velocity = Vector2.ZERO
+	should_process_input = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+
---
+++ Scripts/ground.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+class_name ground
+
+signal bird_crashed
+
+@export var speed = -150
+
+@onready var ground_1 = $Ground1
+@onready var ground_2 = $Ground2
+
+@onready var sprite1 = $Ground1/Sprite1
+@onready var sprite2 = $Ground2/Sprite2
+
+func _ready():
+	sprite2.global_position.x = sprite1.global_position.x + sprite1.texture.get_width()
+	
+func _process(delta):
+	sprite1.global_position.x += speed * delta
+	sprite2.global_position.x += speed * delta
+	
+	if sprite1.global_position.x < -sprite1.texture.get_width():
+		sprite1.global_position.x = sprite2.global_position.x + sprite2.texture.get_width()
+		
+	if sprite2.global_position.x < -sprite2.texture.get_width():
+		sprite2.global_position.x = sprite1.global_position.x + sprite1.texture.get_width()
+
+
+func _on_ground_entered(body):
+	bird_crashed.emit()
+	stop()
+	(body as BirdCharacter).stop()
+	
+func stop():
+	speed = 0
